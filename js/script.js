35c1dc775f6ecf1120d2189c0549e5356c3b3547
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-07-30 19:45:04 +0530

    Updated index.html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        appendDots: '.sliderDots',
+        dotsClass: 'dots',
+
+        autoplay: true,
+        autoplaySpeed: 4000,
+
+    });
+
+});
