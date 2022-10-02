383bcabdc035a0bbf277e1715cf964da0e7daf33
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-10-02 11:55:58 +0530

    New commit

--- js/linkedinBadge.js
+++ js/linkedinBadge.js
@@ -1,3 +1,12 @@
+var iframe = document.getElementsByTagName("iframe")[0];
+var frm = iframe.contentDocument;
+var otherhead = frm.getElementsByTagName("head")[0];
+var link = frm.createElement("link");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("type", "text/css");
+link.setAttribute("href", "css/linkedinBadge.css");
+otherhead.appendChild(link);
+
 
 var aboutMe;
 
