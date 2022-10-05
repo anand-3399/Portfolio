16fa22db1df6266b0ae2d7f4d5fc2823da7396b9
Anand Jaiswar <anandjaiswar02@gmail.com>
2022-10-05 11:22:36 +0530

    New commit

--- js/linkedinBadge.js
+++ js/linkedinBadge.js
@@ -1,14 +1,24 @@
-var iframe = document.getElementsByTagName("iframe")[0];
-var frm = iframe.contentDocument;
-var otherhead = frm.getElementsByTagName("head")[0];
-var link = frm.createElement("link");
-link.setAttribute("rel", "stylesheet");
-link.setAttribute("type", "text/css");
-link.setAttribute("href", "css/linkedinBadge.css");
-otherhead.appendChild(link);
+function addCssToIframesDocument() {
+    var iframe = document.getElementsByTagName("iframe")[0];
+    var frm = iframe.contentDocument;
+    var otherhead = frm.getElementsByTagName("head")[0];
+    var link = frm.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("type", "text/css");
+    link.setAttribute("href", "css/linkedinBadge.css");
+    otherhead.appendChild(link);
+}
 
+function addJStoIframesDocument() {
+    var iframe = document.getElementsByTagName("iframe")[0];
+    var frm = iframe.contentDocument;
+    var aboutMe;
+    aboutMe = frm.getElementsByClassName("profile-badge__content-profile-headline")[0].innerText;
+    console.log(aboutMe);
+    localStorage.setItem('TextInAboutME', aboutMe);
+}
 
-var aboutMe;
+/*
 
 function getAboutMe() {
     aboutMe = document.getElementsByClassName("profile-badge__content-profile-headline")[0].innerText;
@@ -18,3 +28,7 @@ function getAboutMe() {
 
 setTimeout(getAboutMe, 5000);
 
+*/
+
+setTimeout(addCssToIframesDocument, 1000);
+setTimeout(addJStoIframesDocument, 1000);
